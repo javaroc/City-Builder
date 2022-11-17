f8e3236189be31cef5df8d6c6d12dee168c096d9
Alyssa Rogers <rogaly658@gmail.com>
2022-11-17 01:57:05 +0000

    Build basic layout of main page.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class City extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: 'Name Me', money: 0, population: 0, day: 0};
+    this.advanceStep = this.advanceStep.bind(this);
+    this.buildFactory = this.buildFactory.bind(this);
+    this.buildHouse = this.buildHouse.bind(this);
+    this.endGame = this.endGame.bind(this);
+  }
+  
+  advanceStep(event) {
+    this.setState({money: this.state.money + 100});
+    this.setState({day: this.state.day + 1});
+  }
+  
+  buildFactory(event) {
+    this.setState({money: this.state.money - 500});
+  }
+  
+  buildHouse(event) {
+    this.setState({money: this.state.money - 200 });
+    this.setState({population: this.state.population +5});
+  }
+  
+  endGame(event) {
+    //TODO request to server
+  }
+  
+  
+  
+  render() {
+    return (
+    <div class='content'>
+      <button> <a href ='/stats.html'>Stats</a> </button>
+      <div class='game-window'>
+        <h2>City Builder</h2>
+        <div class="current-game">
+          <h3>{this.state.name}</h3>
+          <hr/>
+          <div class="buildings-and-money">
+            <div class="buildings">
+              <p>BUILDINGS: </p>
+            </div>
+            <div class="money">
+              <p>MONEY: {this.state.money} coins</p>
+              <p>POPULATION: {this.state.population} people</p>  
+            </div>
+          </div>
+          <div class="build">
+            <button onClick={this.buildFactory}>Build factory</button>
+            <button onClick={this.buildHouse}>Build house</button>
+          </div>
+        </div>
+      </div>
+      <div class='footer-buttons'>
+        <button onClick={this.advanceStep}>Next day</button>
+        <button onClick={this.endGame}>End game</button>
+      </div>
+      <footer>
+      <p>Site made by Zach Hacking and Alyssa Rogers</p>
+      </footer>
     </div>
-  );
+  
+    );
+  }
 }
 
-export default App;
+export default City;
